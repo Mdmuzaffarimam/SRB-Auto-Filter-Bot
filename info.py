3de9241fd9a42b30d31811fcff5d55faaaa3c9b8
Mdmuzaffarimam <muzaffarimam1@yahoo.com>
2025-07-20 13:52:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'media_search')
 API_ID = int(environ.get('API_ID', '23631217'))
 API_HASH = environ.get('API_HASH', '567c6df308dc6901790309499f729d12')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7694108427:AAEaii1gqBW4_0k3AN9PdOc7io78JmvQdHU")
 
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
